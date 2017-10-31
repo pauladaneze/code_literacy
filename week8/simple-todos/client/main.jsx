f36e835b13866ecdd5d2757af9ddc94243f1fc18
pauladaneze <32017220+pauladaneze@users.noreply.github.com>
2017-10-31 12:00:26 -0400

    Add files via upload

---
+++ week8/simple-todos/client/main.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import '../imports/startup/accounts-config.js';
+
+import App from '../imports/ui/App.jsx';
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
+});
